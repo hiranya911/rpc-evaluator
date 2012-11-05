2157573ca2122da3294859c0130b6b49db497873
Vegar Engen <vegar.engen@gmail.com>
2012-11-05 13:35:05 -0800

    Added jsonrpc

---
+++ jsonrpc-service/src/main/java/JsonServerImpl.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+
+import edu.ucsb.cs.rpc.base.DataObject;
+import edu.ucsb.cs.rpc.base.Server;
+
+
+public class JsonServerImpl implements Server {
+
+	public void doNothing() {
+	}
+
+	public String echoString(String s) {
+		return s;
+	}
+
+	public int echoInt(int i) {
+		return i;
+	}
+
+	public int[] echoArray(int[] array) {
+		return array;
+	}
+
+	public DataObject echoObject(DataObject o) {
+		return o;
+	}
+
+	public Map<Integer, Integer> echoMap(Map<Integer, Integer> map) {
+		return map;
+	}
+
+	public byte[] echoBlob(byte[] blob) {
+		return blob;
+	}
+
+}
