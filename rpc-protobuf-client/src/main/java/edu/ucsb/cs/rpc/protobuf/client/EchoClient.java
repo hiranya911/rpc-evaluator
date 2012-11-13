afa2d5caf063baf1179e41303378e4e869ee5753
Michael Agun <mikeagun@gmail.com>
2012-11-13 10:47:57 -0800

    Finished Protobuf implementation. Note that Google Protocol Buffers doesn't have an implementation of a Map, so that is just stored as two arrays of integers. in this version no marshalling is included in the timing, to be adjusted in the future once we decide what marshalling should be included. also made some minor code changes to the rmi-server.

--- rpc-protobuf-client/src/main/java/edu/ucsb/cs/rpc/protobuf/client/EchoClient.java
+++ rpc-protobuf-client/src/main/java/edu/ucsb/cs/rpc/protobuf/client/EchoClient.java
@@ -4,7 +4,11 @@ import edu.ucsb.cs.rpc.base.Client;
 import edu.ucsb.cs.rpc.base.DataObject;
 import edu.ucsb.cs.rpc.base.InvocationResult;
 import edu.ucsb.cs.rpc.base.RPCEvaluatorException;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoArray;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoBlob;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoDataObject;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoInt;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoMap;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoService;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoService.BlockingInterface;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoString;
@@ -19,24 +23,33 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.concurrent.Executors;
 
 import org.jboss.netty.channel.ChannelFactory;
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import com.google.protobuf.ByteString;
 import com.google.protobuf.RpcController;
 import com.google.protobuf.ServiceException;
 import com.googlecode.protobuf.pro.duplex.PeerInfo;
 import com.googlecode.protobuf.pro.duplex.RpcClientChannel;
 import com.googlecode.protobuf.pro.duplex.client.DuplexTcpClientBootstrap;
+import com.googlecode.protobuf.pro.duplex.execute.SameThreadExecutor;
 import com.googlecode.protobuf.pro.duplex.execute.ThreadPoolCallExecutor;
 
 public class EchoClient implements Client {
 
-    public static final String SERVICE_ENDPOINT = "ProtoBuf.Endpoint";
-    public static final String RMI_REGISTRY_NAME = "Echo";
-    public static final String POLICY_FILE_NAME = "allow_all.policy";
+    public static final String SERVICE_ENDPOINT = "ProtoBuf.ServerEndpoint";
+    public static final String SERVICE_PORT = "ProtoBuf.ServerPort";
+    public static final String CLIENT_ENDPOINT = "ProtoBuf.ClientEndpoint";
+    public static final String CLIENT_COMPRESSION = "ProtoBuf.UseCompression";
+    public static final String CLIENT_BUFFER = "ProtoBuf.ClientReceiveBufferSize";
+    public static final String CLIENT_TCPNODELAY = "ProtoBuf.ClientTcpNoDelay";
+    
     PeerInfo client;
     PeerInfo server;
     DuplexTcpClientBootstrap bootstrap;
@@ -44,6 +57,10 @@ public class EchoClient implements Client {
     BlockingInterface echoService;
     RpcController controller;
     
+    static {
+        System.setProperty("org.apache.commons.logging.Log",
+                           "org.apache.commons.logging.impl.NoOpLog");
+     }
 
 	/*public static void main(String[] args)
 	{
@@ -56,17 +73,52 @@ public class EchoClient implements Client {
 			e.printStackTrace();
 		}
 	}*/
+    
+    private static String getString(Properties properties, String name) throws RPCEvaluatorException
+    {
+    	String value = properties.getProperty(name);
+        if (value == null || "".equals(value)) {
+            throw new RPCEvaluatorException(name + " parameter not specified");
+        }
+        return value;
+    }
+    
+    private static int getInt(Properties properties, String name) throws RPCEvaluatorException
+    {
+    	String valueString = getString(properties, name);
+    	int value = 0;
+    	try
+		{
+    		value = Integer.parseInt(valueString);
+		}
+		catch (NumberFormatException ex)
+		{
+			throw new RPCEvaluatorException("couldn't parse " + name + " parameter to int");
+		}
+    	return value;
+    }
+    
+    private static boolean getBool(Properties properties, String name) throws RPCEvaluatorException
+    {
+    	String valueString = getString(properties, name);
+    	if (valueString.compareToIgnoreCase("true") != 0 && valueString.compareToIgnoreCase("false") != 0)
+    			throw new RPCEvaluatorException("couldn't parse " + name + " parameter to bool");
+    	return Boolean.parseBoolean(valueString);
+    }
 	
     @Override
     public void init(Properties properties) throws RPCEvaluatorException {
-    	String clientHost = "localhost";
-    	String serverHost = "localhost";
-    	int serverPort = 8080;
+    	String clientHost = getString(properties, CLIENT_ENDPOINT);
+    	String serverHost = getString(properties, SERVICE_ENDPOINT);
+    	int serverPort = getInt(properties, SERVICE_PORT);
+    	boolean useCompression = getBool(properties, CLIENT_COMPRESSION);
+    	int receiveBuffer = getInt(properties, CLIENT_BUFFER);
+    	boolean tcpNoDelay = getBool(properties, CLIENT_TCPNODELAY);
     	
     	try {
 	    	client = new PeerInfo(clientHost, 1234);
 	        server = new PeerInfo(serverHost, serverPort);
-	        ThreadPoolCallExecutor executor = new ThreadPoolCallExecutor(3, 10);
+	        SameThreadExecutor executor = new SameThreadExecutor();
 	
 	        bootstrap = new DuplexTcpClientBootstrap(
 	                        client, 
@@ -74,11 +126,11 @@ public class EchoClient implements Client {
 	                Executors.newCachedThreadPool(),
 	                Executors.newCachedThreadPool()),
 	                executor);
-	        bootstrap.setCompression(true);
+	        bootstrap.setCompression(useCompression);
 	        bootstrap.setOption("connectTimeoutMillis",10000);
 	        bootstrap.setOption("connectResponseTimeoutMillis",10000);
-	        bootstrap.setOption("receiveBufferSize", 1048576);
-	        bootstrap.setOption("tcpNoDelay", false);
+	        bootstrap.setOption("receiveBufferSize", receiveBuffer);
+	        bootstrap.setOption("tcpNoDelay", tcpNoDelay);
 	        
 	        channel = bootstrap.peerWith(server);
 	        
@@ -133,9 +185,10 @@ public class EchoClient implements Client {
     public InvocationResult doNothing() {
         Throwable t = null;
 
+    	NothingMessage request = NothingMessage.newBuilder().build();
+    	
         long start = System.currentTimeMillis();
         try {
-        	NothingMessage request = NothingMessage.newBuilder().build();
         	echoService.doNothing(controller, request);
         } catch (Exception e) {
             t = e;
@@ -150,13 +203,17 @@ public class EchoClient implements Client {
         Throwable t = null;
         EchoString response = null;
         
+    	EchoString request = EchoString.newBuilder().setData(s).build();
+    	
         long start = System.currentTimeMillis();
         try {
-        	EchoString request = EchoString.newBuilder().setData(s).build();
 			response = echoService.echoString(controller, request);
         } catch (ServiceException e) {
             t = e;
         }
+	    catch (Exception e) {
+	        t = e;
+	    }
         long end = System.currentTimeMillis();
         	
         if (t == null && (response == null || !response.getData().equals(s))) {
@@ -170,9 +227,10 @@ public class EchoClient implements Client {
     	Throwable t = null;
         EchoInt response = null;
         
+        EchoInt request = EchoInt.newBuilder().setData(i).build();
+        
         long start = System.currentTimeMillis();
         try {
-        	EchoInt request = EchoInt.newBuilder().setData(i).build();
 			response = echoService.echoInt(controller, request);
         } catch (Exception e) {
             t = e;
@@ -188,11 +246,14 @@ public class EchoClient implements Client {
     @Override
     public InvocationResult echoArray(int[] array) {
     	Throwable t = null;
-        int[] response = null;
-        
+        EchoArray response = null;
+    	edu.ucsb.cs.rpc.protobuf.Echo.EchoArray.Builder requestBuilder = EchoArray.newBuilder();
+    	for(int i = 0; i < array.length; i++)
+    		requestBuilder.addData(array[i]);
+    	EchoArray request = requestBuilder.build();
         long start = System.currentTimeMillis();
         try {
-            //response = echo.echoArray(array);
+            response = echoService.echoArray(controller, request);
         } catch (Exception e) {
             t = e;
         }
@@ -207,11 +268,16 @@ public class EchoClient implements Client {
     @Override
     public InvocationResult echoObject(DataObject o) {
     	Throwable t = null;
-    	DataObject response = null;
-        
+    	EchoDataObject response = null;
+    	edu.ucsb.cs.rpc.protobuf.Echo.EchoDataObject.Builder requestBuilder = EchoDataObject.newBuilder();
+    	requestBuilder.setChar(o.getCharacter());
+    	requestBuilder.setDecimal(o.getDecimal());
+    	requestBuilder.setInt(o.getInteger());
+    	requestBuilder.setString(o.getString());
+    	EchoDataObject request = requestBuilder.build();
         long start = System.currentTimeMillis();
         try {
-            //response = echo.echoObject(o);
+        	response = echoService.echoDataObject(controller, request);
         } catch (Exception e) {
             t = e;
         }
@@ -226,11 +292,16 @@ public class EchoClient implements Client {
     @Override
     public InvocationResult echoMap(Map<Integer, Integer> map) {
     	Throwable t = null;
-    	Map<Integer, Integer> response = null;
-        
+    	EchoMap	response = null;
+    	edu.ucsb.cs.rpc.protobuf.Echo.EchoMap.Builder requestBuilder = EchoMap.newBuilder();
+    	for (Entry<Integer, Integer> entry : map.entrySet()) {
+    	    requestBuilder.addKeys(entry.getKey());
+    	    requestBuilder.addValues(entry.getValue());
+    	}
+    	EchoMap request = requestBuilder.build();
         long start = System.currentTimeMillis();
         try {
-            //response = echo.echoMap(map);
+        	response = echoService.echoMap(controller, request);
         } catch (Exception e) {
             t = e;
         }
@@ -245,11 +316,13 @@ public class EchoClient implements Client {
     @Override
     public InvocationResult echoBlob(byte[] blob) {
     	Throwable t = null;
-    	byte[] response = null;
-        
+    	EchoBlob response = null;
+    	
+    	EchoBlob request = EchoBlob.newBuilder().setData(ByteString.copyFrom(blob)).build();
+    	
         long start = System.currentTimeMillis();
         try {
-            //response = echo.echoBlob(blob);
+			response = echoService.echoBlob(controller, request);
         } catch (Exception e) {
             t = e;
         }
--- rpc-protobuf-service/src/main/java/edu/ucsb/cs/rpc/protobuf/ProtobufEchoServer.java
+++ rpc-protobuf-service/src/main/java/edu/ucsb/cs/rpc/protobuf/ProtobufEchoServer.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs.rpc.protobuf;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,11 +11,14 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.Executors;
 
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 import edu.ucsb.cs.rpc.base.DataObject;
+import edu.ucsb.cs.rpc.base.RPCEvaluatorException;
+
 import com.google.protobuf.ByteString;
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
@@ -23,25 +27,52 @@ import com.googlecode.protobuf.pro.duplex.execute.RpcServerCallExecutor;
 import com.googlecode.protobuf.pro.duplex.execute.ThreadPoolCallExecutor;
 import com.googlecode.protobuf.pro.duplex.server.DuplexTcpServerBootstrap;
 
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoArray;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoBlob;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoDataObject;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoInt;
+import edu.ucsb.cs.rpc.protobuf.Echo.EchoMap;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoService;
 import edu.ucsb.cs.rpc.protobuf.Echo.EchoString;
 import edu.ucsb.cs.rpc.protobuf.Echo.NothingMessage;
 
 public class ProtobufEchoServer extends EchoService {
 	DuplexTcpServerBootstrap bootstrap;
-
+	public static final String SERVICE_ENDPOINT = "ProtoBuf.ServerEndpoint";
+    public static final String SERVICE_PORT = "ProtoBuf.ServerPort";
 	
-	public ProtobufEchoServer()
+    static {
+        System.setProperty("org.apache.commons.logging.Log",
+                           "org.apache.commons.logging.impl.NoOpLog");
+     }
+    
+    private static String getString(Properties properties, String name) throws RPCEvaluatorException
+    {
+    	String value = properties.getProperty(name);
+        if (value == null || "".equals(value)) {
+            throw new RPCEvaluatorException(name + " parameter not specified");
+        }
+        return value;
+    }
+    
+    private static int getInt(Properties properties, String name) throws RPCEvaluatorException
+    {
+    	String valueString = getString(properties, name);
+    	int value = 0;
+    	try
+		{
+    		value = Integer.parseInt(valueString);
+		}
+		catch (NumberFormatException ex)
+		{
+			throw new RPCEvaluatorException("couldn't parse " + name + " parameter to int");
+		}
+    	return value;
+    }
+    
+	public ProtobufEchoServer(String hostName, int port)
 	{
-		String name = "localhost";
-		int port = 8080;
-		/*if (args.length > 0)
-			name = args[0];
-		if (args.length > 1)
-			port = Integer.parseInt(args[1]);*/
-		
-		PeerInfo serverInfo = new PeerInfo(name, port);
+		PeerInfo serverInfo = new PeerInfo(hostName, port);
 		 RpcServerCallExecutor executor = new ThreadPoolCallExecutor(10, 10);
 	        
 		 bootstrap = new DuplexTcpServerBootstrap(
@@ -59,6 +90,7 @@ public class ProtobufEchoServer extends EchoService {
     				{
     					try 
     					{
+    						System.out.println("Shutting down EchoService");
     						bootstrap.releaseExternalResources();
     					}
     					catch (Exception e)
@@ -72,8 +104,35 @@ public class ProtobufEchoServer extends EchoService {
 	
 	public static void main(String[] args)
 	{
-		ProtobufEchoServer server = new ProtobufEchoServer();
 		
+		Properties properties = new Properties();
+		try {
+            properties.load(new FileInputStream(args[0]));
+        } catch (IOException e) {
+            System.out.println("Error loading properties file");
+            printUsage();
+            return;
+        }
+		String serverHost;
+		int serverPort;
+		try {
+			serverHost = getString(properties, SERVICE_ENDPOINT);
+			serverPort = getInt(properties, SERVICE_PORT);
+		} catch (RPCEvaluatorException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.toString());
+			printUsage();
+            return;
+		}
+    	
+    	System.out.println("Starting EchoService on " + serverHost + ":" + serverPort);
+		ProtobufEchoServer server = new ProtobufEchoServer(serverHost, serverPort);
+		
+	}
+	
+	private static void printUsage()
+	{
+		System.out.println("Usage: java edu.ucsb.cs.rpc.protobuf.ProtobufEchoServer popertiesfile");
 	}
 	
 	/*public String echoString(String s) {
@@ -85,49 +144,71 @@ public class ProtobufEchoServer extends EchoService {
 		return i;
 	}*/
 
-	public int[] echoArray(int[] array) {
+	/*public int[] echoArray(int[] array) {
 		// TODO Auto-generated method stub
 		return array;
-	}
+	}*/
 
-	public DataObject echoObject(DataObject o) {
+	/*public DataObject echoObject(DataObject o) {
 		// TODO Auto-generated method stub
 		return o;
-	}
+	}*/
 
 	public Map<Integer, Integer> echoMap(Map<Integer, Integer> map) {
 		// TODO Auto-generated method stub
 		return map;
 	}
 
-	public byte[] echoBlob(byte[] blob) {
+	/*public byte[] echoBlob(byte[] blob) {
 		// TODO Auto-generated method stub
 		return blob;
 	}
     
 	public void doNothing(){
 		return;
-	}
+	}*/
 
 	@Override
 	public void echoString(RpcController controller, EchoString request,
 			RpcCallback<EchoString> done) {
-		EchoString response = EchoString.newBuilder().setData(request.getData()).build();
-		done.run(response);
+		done.run(request);
 	}
 
 	@Override
 	public void echoInt(RpcController controller, EchoInt request,
 			RpcCallback<EchoInt> done) {
-		EchoInt response = EchoInt.newBuilder().setData(request.getData()).build();
-		done.run(response);
+		done.run(request);
 	}
 
 	@Override
 	public void doNothing(RpcController controller, NothingMessage request,
 			RpcCallback<NothingMessage> done) {
-		NothingMessage response = NothingMessage.newBuilder().build();
-		done.run(response);
+		done.run(request);
 		
+	}
+
+	@Override
+	public void echoBlob(RpcController controller, EchoBlob request,
+			RpcCallback<EchoBlob> done) {
+		done.run(request);
+	}
+
+	@Override
+	public void echoDataObject(RpcController controller,
+			EchoDataObject request, RpcCallback<EchoDataObject> done) {
+		done.run(request);
+		
+	}
+
+	@Override
+	public void echoArray(RpcController controller, EchoArray request,
+			RpcCallback<EchoArray> done) {
+		done.run(request);
+	}
+
+	@Override
+	public void echoMap(RpcController controller, EchoMap request,
+			RpcCallback<EchoMap> done) {
+		done.run(request);
 	}
 }
